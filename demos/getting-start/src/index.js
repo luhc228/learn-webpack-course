6e1b8d4eea3f8a0fba89f5aa9f853d598283d559
luhc228 <luhengchang228@126.com>
2023-01-07 16:43:27 +0800

    add getting-start demo

---
+++ demos/getting-start/src/index.js
@@ -0,0 +1,5 @@
+function add(a, b) {
+  return a + b;
+}
+
+console.log('Add 1 + 2 = ', add(1, 2));
---
+++ demos/getting-start/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+/**
+ * @type {import('webpack').Configuration}.config
+ */
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+};
