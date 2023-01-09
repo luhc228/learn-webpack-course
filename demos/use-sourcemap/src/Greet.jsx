73ccc5c213d215adb86520c1e92aa9fe4fe933c3
luhc228 <luhengchang228@126.com>
2023-01-09 20:35:57 +0800

    feat: use sourcemap

---
+++ demos/use-sourcemap/src/Greet.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import './greet.css';
+
+const Greet = () => {
+  throw new Error('xxx')
+  return (
+    <>
+      <h1 className='title'>
+        Hello, World!
+      </h1>
+      <br />
+      Your Name: <input />
+    </>
+  )
+}
+
+export default Greet;
---
+++ demos/use-sourcemap/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+const isDevelopment = false;
+
+/**
+ * @type {import('webpack').Configuration}.config
+ */
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: isDevelopment ? 'development' : 'production',
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: [isDevelopment && require.resolve('react-refresh/babel')].filter(Boolean),
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    isDevelopment && new ReactRefreshWebpackPlugin(),
+  ].filter(Boolean),
+  devServer: {
+    hot: true,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+    },
+  },
+};
